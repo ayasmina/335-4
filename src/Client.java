7aeccc15d6158d68bdbcdf306e1d013ad21c634b
Lynn Cavanagh <elliecavanagh002@gmail.com>
2024-11-20 23:29:37 -0800

    Cleaned up code and added the latest work!

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,10 @@
-import java.io.IOException;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.regex.*;
 
 public class Client {
-    private String HOST;
+    private static String HOST;
     private static final int PORT = 8000;
     private boolean isConnected = false;
     private boolean isLoggedIn = false;
@@ -13,65 +15,105 @@ public class Client {
     private boolean passValid = false;
     private boolean emailValid = false;
     private Socket socket;
+    private static Network clientConnection;
 
+    // Connect Client to Client Network Object
     public Client(String host) {
-        try {
-            this.HOST = host;
-            socket = new Socket(HOST, PORT);
-        } catch (UnknownHostException | IOException e) {
-            System.out.println("Host: " + HOST + " at port " + PORT + " + is unavailable.");
+        Client.HOST = host;
+       clientConnection = new Network(HOST);
+    }
+
+    public void connect() {
+        if (!isConnected) {
+            isConnected = true;
+            System.out.println("Connected to " + HOST);
+        } else {
+            System.out.println("Already connected.");
         }
+    }
+
+    public void disconnect() {
+        if (isConnected) {
+            isConnected = false;
+            isLoggedIn = false;  // Automatically log out on disconnect
+            System.out.println("Disconnected from " + HOST);
+
+        } else {
+            System.out.println("Not connected.");
+        }
+    }
+
+    public void login(String username, String password) {
+        if (isConnected && !isLoggedIn) {
+            isLoggedIn = true;
+            System.out.println("User " + username + " logged in.");
+        } else if (!isConnected) {
+            System.out.println("Please connect to the server first.");
+        } else {
+            System.out.println("Already logged in.");
+        }
+    }
+
+    public void logout() {
+        if (isLoggedIn) {
+            isLoggedIn = false;
+            System.out.println("Logged out.");
+        } else {
+            System.out.println("Not logged in.");
+        }
+    }
+ //Needs pass and email validation
+    public void register(String username, String password, String email) {
+        if (isConnected) {
+            System.out.println("User " + username + " registered with email " + email);
+        } else {
+            System.out.println("Please connect to the server first.");
+        }
+    }
+    // New recoverPassword method
+    public void recoverPassword(String username) {
+        if (isConnected) {
+            // Simulate sending a temporary password to the user's registered email
+            String tempPassword = "Temp1234"; // Temporary password for demonstration purposes
+            System.out.println("Temporary password sent to the email associated with username: " + username);
+            System.out.println("Temporary password: " + tempPassword); // In real applications, do not log sensitive data
+        } else {
+            System.out.println("Please connect to the server first.");
+        }
+    }
+
+    // Connection status method
+    public void printConnectionStatus() {
+        System.out.println("Server: " + HOST);
+        System.out.println("Connected: " + isConnected);
+        System.out.println("Logged In: " + isLoggedIn);
+    }
+
+    public static void main(String[] args){
+        String replyString2;
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Server IP Address: ");
+        String serverIP = kb.next();
+        Client client = new Client(serverIP);
+        System.out.print("Input Username: ");
+        StringBuilder username = new StringBuilder();
+        String input = kb.next();
+        username.append('U').append(input);
+        String replyString = clientConnection.sendString(username.toString());
+        while(replyString.equals("Valid username!")){
+            System.out.print("Input password: ");
+            StringBuilder password = new StringBuilder();
+            input = kb.next();
+            password.append('P').append(username.deleteCharAt(0).append(input));
+            System.out.print(username + " " + password);
+            replyString2 = clientConnection.sendString(password.toString());
+            System.out.println(replyString + " " + replyString2);
+            replyString = "";
+        }
+        kb.close();
     }
 }
 
-//    public void connect() {
-//        if (!isConnected) {
-//            isConnected = true;
-//            System.out.println("Connected to " + HOST);
-//        } else {
-//            System.out.println("Already connected.");
-//        }
-//    }
-
-//    public void disconnect() {
-//        if (isConnected) {
-//            isConnected = false;
-//            isLoggedIn = false;  // Automatically log out on disconnect
-//            System.out.println("Disconnected from " + HOST);
-//
-//        } else {
-//            System.out.println("Not connected.");
-//        }
-//    }
-
-//    public void login(String username, String password) {
-//        if (isConnected && !isLoggedIn) {
-//            isLoggedIn = true;
-//            System.out.println("User " + username + " logged in.");
-//        } else if (!isConnected) {
-//            System.out.println("Please connect to the server first.");
-//        } else {
-//            System.out.println("Already logged in.");
-//        }
-//    }
-
-//    public void logout() {
-//        if (isLoggedIn) {
-//            isLoggedIn = false;
-//            System.out.println("Logged out.");
-//        } else {
-//            System.out.println("Not logged in.");
-//        }
-//    }
-// Needs pass and email validation
-//    public void register(String username, String password, String email) {
-//        if (isConnected) {
-//            System.out.println("User " + username + " registered with email " + email);
-//        } else {
-//            System.out.println("Please connect to the server first.");
-//        }
-//    }
-//}
-//add forgot password ??????!!!!!!!
 //once u log in
 //whos on how many acc are logged in how many accounts are not/ connection status
+
--- src/Network.java
+++ src/Network.java
@@ -1,63 +1,60 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+
+// Client: Send, then Receive/listen
+// Server: Receive/Listen then Send
+
+// Need to get the peerconnection and listen methods in this somehow
 
 public class Network extends Thread {
+    private static final int PORT = 8000;
+
     private boolean go;
     private String name;
     private int id;
-    private int nextId = 0;
+    private int nextId;
+    private boolean clientIsConnected = false;
 
-    // handling peer to peer communication
+    // handling peer to peer communication - I/O Streams
     private BufferedReader datain;
     private DataOutputStream dataout;
 
     private Server server;
     private Socket socket;
-    private static final int PORT = 8000;
 
-    public int getPort() {
-        return PORT;
+    // Create general network objects??
+    // Client Network Object holds socket and I/O streams?
+    // Server Network Object holds NetworkThread? - Do we need to put a NetworkThread class in here?
+
+    // In given code: this was the public Client() method in Client.java
+    public Network(String host){
+        try {
+            // -- construct the peer to peer socket
+            socket = new Socket(host, PORT);
+            // -- wrap the socket in stream I/O objects
+            datain = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            dataout = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException e) {
+            System.out.println("Host " + host + " at port " + PORT + " is unavailable.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("Unable to create I/O streams.");
+            System.exit(1);
+        }
     }
 
+    //In given code: this was the ConnectionThread Constructor
+    // -- creates I/O objects on top of the socket
     public Network(int id, Socket socket, Server server) {
         this.server = server;
         this.id = id;
         this.name = Integer.toString(id);
         go = true;
+        clientIsConnected = true;
 
         try {
             datain = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             dataout = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    public String toString() {
-        return name;
-    }
-
-    public String getname() {
-        return name;
-    }
-
-
-    public void peerconnection(Socket socket) {
-
-    }
-
-    public void disconnect() {
-        String text = "disconnect\n";
-        try {
-            // sending message to let server know client is disconnecting
-            dataout.writeBytes(text);
-            dataout.flush();
-            // closing peer to peer socket
-            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -84,64 +81,93 @@ public class Network extends Thread {
         return rtnmsg;
     }
 
-    private void listen() {
-        try {
-            serverSocket = new ServerSocket(getPort());
+    //    public String toString() {
+//        return name;
+//    }
 
-            while (true) {
-                Socket socket = serverSocket.accept();
+//    public String getname() {
+//        return name;
+//    }
 
-                peerconnection(socket);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
 
-    public void removeID(int id) {
-        // -- find the object belonging to the client thread being terminated
-        for (int i = 0; i < clientconnections.size(); ++i) {
-            Network cc = clientconnections.get(i);
-            long x = cc.getId();
-            if (x == id) {
-                // -- remove it from the active threads list
-                //    the thread will terminate itself
-                clientconnections.remove(i);
-
-                // -- place some text in the area to let the server operator know
-                //    what is going on
-                System.out.println("SERVER: connection closed for client id " + id + "\n");
-                break;
-            }
-        }
-    }
-
-    public void run() {
-        while (go) {
+//    public void run() {
+//        while (go) {
+//            try {
+//                String txt = datain.readLine();
+//                System.out.println("SERVER receive: " + txt);
+//
+//                if (txt.equals("disconnect")) {
+//                    datain.close();
+//                    server.removeID(id);
+//                    go = false;
+//                } else if (txt.equals("hello")) {
+//
+//                    dataout.writeBytes("world!" + "\n");
+//                    dataout.flush();
+//
+//                } else {
+//                    System.out.println("unrecognized command >>" + txt + "<<");
+//                    dataout.writeBytes(txt + "\n");
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                go = false;
+//            }
+//        }
+//
+//    }
+    // Pablo's run code
+    public void run () {
+        // Server thread runs until the client terminates the connection
+        while (clientIsConnected) {
             try {
+                /*  always receives a String object with a newline (\n)
+                    on the end due to how BufferedReader readLine() works.
+                    The client adds it to the user's string but the BufferedReader
+                    readLine() call strips it off   */
                 String txt = datain.readLine();
+                String txtOut = Server.parseInput(txt);
                 System.out.println("SERVER receive: " + txt);
+                // -- if it is not the termination message, send it back adding the
+                //    required (by readLine) "\n"
 
-                if (txt.equals("disconnect")) {
+                //  If the disconnect string is received then close the socket, remove this thread object from the
+                //  server's active client thread list, and terminate the thread
+                if (txtOut.equals("disconnect")) {
                     datain.close();
-                    Network.removeID(id);
-                    go = false;
-                } else if (txt.equals("hello")) {
-
-                    dataout.writeBytes("world!" + "\n");
+                    //Server.removeID(id); // Error calling removeID()
+                    clientIsConnected = false;
+                }   //  End If
+                else if (txtOut.equals("validUsername")) {
+                    dataout.writeBytes("Valid username!" + "\n");
                     dataout.flush();
-
-                } else {
+                }   //  End Else If
+                else if (txtOut.equals("validPassword")) {
+                    dataout.writeBytes("You have successfully logged in!");
+                    dataout.flush();
+                }   //  End Else If
+                else if(txtOut.equals("wrongUsername")){
+                    dataout.writeBytes("Unrecognized username.");
+                    dataout.flush();
+                }
+                else if(txtOut.equals("wrongPassword")){
+                    dataout.writeBytes("Incorrect password.");
+                    dataout.flush();
+                }
+                else if(txtOut.equals("logout")){
+                    dataout.writeBytes("You have successfully logged out!");
+                    dataout.flush();
+                }
+                else {
                     System.out.println("unrecognized command >>" + txt + "<<");
                     dataout.writeBytes(txt + "\n");
-                }
-            } catch (IOException e) {
+                }   //  End Else
+            }   //  End Try
+            catch(IOException e) {
                 e.printStackTrace();
-                go = false;
+                clientIsConnected = false;
             }
         }
-
     }
 }
 
