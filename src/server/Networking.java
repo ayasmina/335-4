ef9352e3a8afa2445713c587bd5af8202ac4f4f2
pablogeorge <pablo@georges.nu>
2024-11-21 00:04:10 -0800

    This is incomplete

--- src/server/Networking.java
+++ src/server/Networking.java
@@ -8,21 +8,18 @@ import java.net.Socket;
 public class Networking extends Thread {
     //	Go is the Logged in Boolean
     private boolean clientIsConnected;
-    private String name;
     private int id;
-
-    // -- the main server (port listener) will supply the socket
-    //    the thread (this class) will provide the I/O streams
-    //    BufferedReader is used because it handles String objects
-    //    whereas DataInputStream does not (primitive types only)
     private BufferedReader datain;
     private DataOutputStream dataout;
     private Server server;
     //  server.Server is a reference to the "parent" Server object which will be set at time of construction
+    // -- the main server (port listener) will supply the socket
+    //    the thread (this class) will provide the I/O streams
+    //    BufferedReader is used because it handles String objects
+    //    whereas DataInputStream does not (primitive types only)
     public Networking(int id, Socket socket, Server server) {
         this.server = server;
         this.id = id;
-        this.name = Integer.toString(id);
         clientIsConnected = true;
         // Create the stream I/O objects on top of the socket
         try {
@@ -33,13 +30,6 @@ public class Networking extends Thread {
             System.exit(1);
         }
     }
-    public String toString () {
-        return name;
-    }
-    public String getname ()
-    {
-        return name;
-    }
     public void run () {
         // Server thread runs until the client terminates the connection
         while (clientIsConnected) {
@@ -48,12 +38,11 @@ public class Networking extends Thread {
                     on the end due to how BufferedReader readLine() works.
                     The client adds it to the user's string but the BufferedReader
                     readLine() call strips it off   */
-                String txt = datain.readLine();
-                String txtOut = Server.parseInput(txt);
-                System.out.println("SERVER receive: " + txt);
+                String txtIn = datain.readLine();
+                String txtOut = Server.parseInput(txtIn);
+                System.out.println("SERVER receive: " + txtIn);
                 // -- if it is not the termination message, send it back adding the
                 //    required (by readLine) "\n"
-
                 //  If the disconnect string is received then close the socket, remove this thread object from the
                 //  server's active client thread list, and terminate the thread
                 if (txtOut.equals("disconnect")) {
@@ -61,30 +50,10 @@ public class Networking extends Thread {
                     Server.removeID(id);
                     clientIsConnected = false;
                 }   //  End If
-                else if (txtOut.equals("validUsername")) {
-                    dataout.writeBytes("Valid username!" + "\n");
-                    dataout.flush();
-                }   //  End Else If
-                else if (txtOut.equals("validPassword")) {
-                    dataout.writeBytes("You have successfully logged in!");
-                    dataout.flush();
-                }   //  End Else If
-                else if(txtOut.equals("wrongUsername")){
-                    dataout.writeBytes("Unrecognized username.");
-                    dataout.flush();
-                }
-                else if(txtOut.equals("wrongPassword")){
-                    dataout.writeBytes("Incorrect password.");
-                    dataout.flush();
-                }
-                else if(txtOut.equals("logout")){
-                    dataout.writeBytes("You have successfully logged out!");
-                    dataout.flush();
-                }
                 else {
-                    System.out.println("unrecognized command >>" + txt + "<<");
-                    dataout.writeBytes(txt + "\n");
-                }   //  End Else
+                    dataout.writeBytes(txtOut + "\n");
+                    dataout.flush();
+                }   //  End Else If
             }   //  End Try
             catch(IOException e) {
                 e.printStackTrace();
--- src/server/Server.java
+++ src/server/Server.java
@@ -5,110 +5,112 @@ import java.net.Socket;
 import java.util.Vector;
 
 public class Server {
+    //  Instantiate list for active client threads
+    public static Vector<Networking> clientConnections;
+    //  Assign each client connection an ID
     private static int nextId = 0;
+    //  Socket waits for client connections
+    public static ServerSocket serversocket;
+    //  Port number used for client communication
+    private static final int PORT = 8000;
+
     public Server() {
-        // Construct the list of active client threads
-        clientconnections = new Vector<>();
-        // Listen for incoming connection requests
+        //  Construct the list of active client threads
+        clientConnections = new Vector<>();
+        //  Listen for incoming connection requests
         listen();
     }
     private void peerConnection(Socket socket) {
-        // Create a thread communication when client arrives
+        //  Create a thread communication when client arrives
         Networking connection = new Networking(nextId, socket, this);
-        // Add the new thread to the active client threads list
-        clientconnections.add(connection);
-        // Start the thread
+        //  Add the new thread to the active client threads list
+        clientConnections.add(connection);
+        //  Start the thread
         connection.start();
-        // Place some text in the area to let the server operator know what is going on
+        //  Place some text in the area to let the server operator know what is going on
         System.out.println("SERVER: connection received for id " + nextId + "\n");
         ++nextId;
     }
+    public void listen(){
+        try {
+            //  Open the server socket
+            serversocket = new ServerSocket(Server.getPort());
+            //  Server runs until we manually shut it down
+            while(true) {   //  Infinite Loop Created
+                //  Block until a client comes along
+                Socket socket = serversocket.accept();
+                //  If connection is accepted then create a peer-to-peer socket
+                peerConnection(socket);
+            }   //  End While (true)
+        }   //  End Try
+        catch(IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    // Called by a ServerThread after a client is terminated
     public static void removeID(int id) {
-        // Find the object belonging to the client thread being terminated
-        for (int i = 0; i < clientconnections.size(); ++i) {
-            Networking cc = clientconnections.get(i);
+        //  Find the object belonging to the client thread being terminated
+        for (int i = 0; i < clientConnections.size(); ++i) {
+            Networking cc = clientConnections.get(i);
             long x = cc.getId();
             if (x == id) {
-                // -- remove it from the active threads list
-                //    the thread will terminate itself
-                clientconnections.remove(i);
-                // Place some text in the area to let the server operator know what is going on
+                // Remove ID from the clientConnections list and the connection thread will terminate itself
+                clientConnections.remove(i);
+                //  Place some text in the area to let the server operator know what is going on
                 System.out.println("SERVER: connection closed for client id " + id + "\n");
                 break;
             }   //  End if(x == id)
         }   //  End removeID(int id)
     }
-    public void listen(){
-        try {
-            // Open the server socket
-            serversocket = new ServerSocket(Server.getPort());
-            // Server runs until we manually shut it down
-            while(true) {  //  Infinite Loop Created
-                // Block until a client comes along
-                Socket socket = serversocket.accept();
-                // If connection is accepted then create a peer-to-peer socket
-                peerConnection(socket);
-            }   //  End While (true)
-
-        }   //  End Try
-        catch(IOException e) {
-            e.printStackTrace();    //  !!!POTENTIAL PROBLEM!!!
-            System.exit(1);
-        }
+    //  Returns Sever Port
+    public static int getPort() {
+        return PORT;
     }
-    public static Vector<Networking> clientconnections;
-
     //  Starts the server
     public static void startServer() {
         // Instantiate the server anonymously
         new Server();
         // No need to keep a reference to the object since it will run in its own thread
     }
+    //  Stops the server
     public static void stopServer() {
         try {
+            //  Attempts to stop the server
             serversocket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    // Assign each client connection an ID
-    // Socket waits for client connections
-    public static ServerSocket serversocket;
-    // Port number used for client communication
-    private static final int PORT = 8000;
-    //  Instantiate list for active client threads
-    public static int getPort() {
-        return PORT;
-    }
-    // Called by a ServerThread after a client is terminated
-
-
     public static String parseInput(String data){
         char operation;
         String result = "";
-        if(data != null){
+        if(data != null) {
             operation = data.charAt(0);
-            result = data.substring(1);
-            if(operation == 'U'){
-                String username = result;
-                if(username.equals("testUsername")){
-                    result = "validUsername";
-                } else {
-                    result = "wrongUsername";
-                }
-            } else if (operation == 'P') {
-                String password = result;
-                if(password.equals("testUsernametestPassword")){
-                    result = "validPassword";
-                } else {
-                    result = "wrongPassword";
-                }
-            } else if (operation == 'D') {
-                result = "disconnect";
-            } else if (operation == 'L') {
-                result = "logout";
-            }
-        }
+            if(data.length() > 1) {
+                result = data.substring(1);
+                switch (operation) {
+                    case 0:
+                        System.out.println("Connect");
+                        break;
+                    case 1:
+                        System.out.println("Login");
+                        break;
+                    case 2:
+                        System.out.println("Register User");
+                        break;
+                    case 3:
+                        System.out.println("Password Recovery");
+                        break;
+                    case 4:
+                        System.out.println("Logout");
+                        break;
+                    case 5:
+                        System.out.println("Disconnect");
+                        break;
+                }   //  End Switch (operation)
+            }   //  End If (data length > 1)
+        }   //  End If (Data is not null)
         return result;
     }
 }
